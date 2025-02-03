c932d63d71111650bc471cbe346ffe2fe66154dd
Chinmay Meghare <chinmaym100@gmail.com>
2025-02-04 00:50:26 +0530

    Second commit for Todo List App made in React

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-To-Do-List-App/',
   plugins: [react()],
-})
+  base: "/React-To-Do-List-App/", // Replace 'your-repo-name' with your actual repository name
+});
